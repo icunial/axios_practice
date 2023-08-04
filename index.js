e8860ebf2d4bda6589be4c33a5990da2b3ae82bc
Ignacio Cunial <igsecu@hotmail.com>
2023-08-04 18:43:12 -0300

    Axios get() shorter form

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const axios = require("axios");
 // Get Request
 
 function getTodos() {
-  axios({
-    method: "get",
-    url: "https://jsonplaceholder.typicode.com/todos",
-  })
+  //   axios({
+  //     method: "get",
+  //     url: "https://jsonplaceholder.typicode.com/todos",
+  //     params: {
+  //       _limit: 5,
+  //     },
+  // })
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
     .then((res) => {
-      console.log(res);
+      console.log(res.data);
     })
     .catch((err) => {
       console.error(err);
